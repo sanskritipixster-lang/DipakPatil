e5452acb3d653c03a3f48e87f0120ea18ddc42c5
Dipak Patil <91220202+DipakPatil007@users.noreply.github.com>
2026-02-06 09:36:54 +0000

    START

---
+++ CurrencyConverter/CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by AressMacMiniA1993 on 06/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
